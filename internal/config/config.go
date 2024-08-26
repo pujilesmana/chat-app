be154804fe92a89a8335d1a550a5e91958ed2a8f
M. Puji Lesmana <puji.lesmana@prodia.co.id>
2024-08-26 15:18:49 +0700

    init config

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUsername string
+	DBPassword string
+	JWTSecret  string
+	BaseURL    string
+	AppPort    string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUsername: os.Getenv("DB_USERNAME"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		JWTSecret:  os.Getenv("JWT_SECRET"),
+		BaseURL:    os.Getenv("BASE_URL"),
+		AppPort:    os.Getenv("APP_PORT"),
+	}
+
+	return config
+}
